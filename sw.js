f7f72017303e30dae583a043bbefc244136081b1
deploy <deploy@localhost>
2026-09-12 00:46:32 +0900

    deploy d347bebcb6 2026-09-12 00:46

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* 設定判別ツール service worker: 同一オリジンはキャッシュ優先、index.html はネット優先。VERSION が変わると入れ替わる */
-var VERSION = 'a2ed58ef04';
+var VERSION = 'd347bebcb6';
 var CACHE = 'snv-' + VERSION;
 var ASSETS = ['./', './index.html', './data.enc', './manifest.webmanifest', './icon-192.png', './icon-512.png', './apple-touch-icon.png'];
 self.addEventListener('install', function(e){ e.waitUntil(caches.open(CACHE).then(function(c){ return c.addAll(ASSETS); }).then(function(){ return self.skipWaiting(); })); });
